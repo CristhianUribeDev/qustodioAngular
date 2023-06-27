d6098c2302e81d5cc5d8f4cc40c5dfe4b851abd1
Cristhian Uribe <cristhianuribedev@outlook.com>
2023-06-27 08:42:37 +0000

    Version Final 2.1

--- src/app/tecnologies/tecnologies.component.ts
+++ src/app/tecnologies/tecnologies.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-tecnologies',
   templateUrl: './tecnologies.component.html',
   styleUrls: ['./tecnologies.component.css']
 })
 export class TecnologiesComponent {
+  basePath: string;
 
+  constructor(private location: Location) {
+    this.basePath = this.location.prepareExternalUrl('assets/images/');
+  }
 }
